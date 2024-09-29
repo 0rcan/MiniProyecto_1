fe74b4c227ec116556d69a93b342b9ddce787aa6
A75TD <tunubala.jesus@correounivalle.edu.co>
2024-09-29 17:35:07 -0500

    iniciando

--- src/Capitan.java
+++ src/Capitan.java
@@ -1,3 +1,7 @@
 public class Capitan {
-    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Funcionaaa");
+        System.out.println("Estamos ready");
+    }
 }
